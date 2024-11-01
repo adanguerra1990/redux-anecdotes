b5878f82aa79b6f9355e0f9cd738e9c5dccf060d
adanguerra1990 <aj90guerra@gmail.com>
2024-11-01 14:02:55 -0400

    Ordenar anecdotas por numeros de votos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   const anecdotes = useSelector((state) => state)
   const dispatch = useDispatch()
 
+  // ordenar anecdotes por numeros de votos
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+
   const addAnecdote = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
@@ -20,7 +23,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
